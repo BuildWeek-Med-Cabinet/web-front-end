966b1fa74257b67e2cbd78edd3311087768bcbef
Parimala Vemula <parimala.thodupunoori@gmail.com>
2020-06-24 16:50:50 -0700

    1.show/hide password 2.fixed br

--- src/components/PrefernecePage/CannabisDetails.js
+++ src/components/PrefernecePage/CannabisDetails.js
@@ -10,10 +10,10 @@ export default function CannabisDetails({ details }) {
     return(
         <div className='div-cannabis'>
             <h1>{details.name} </h1>
-            <p><br>Type:{details.type}</br></p>
-            <p><br>Flavor:{details.flavor}</br></p>
-            <p><br>Effect:{details.effect}</br></p>
-            <p><br>Description:{details.description}</br></p>
+            <p>Type:{details.type}<br/></p>
+            <p>Flavor:{details.flavor}<br/></p>
+            <p>Effect:{details.effect}<br/></p>
+            <p>Description:{details.description}<br/></p>
         </div>
     )
 }
