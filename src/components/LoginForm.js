44f89ce497c0650793df5ecba619c6b856271a47
Parimala Vemula <parimala.thodupunoori@gmail.com>
2020-06-25 15:46:27 -0700

    1. DRY: Deleted CannabisDetails and used CannabisCard instead 2. Styling
    1. Styling LoginForm, SignupForm 2. Redirect / to /recommend if authenticated, else to /login
    
    Fixes/cleanup

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,122 +1,175 @@
 import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import * as Yup from "yup";
 import { loginFormSchema } from "./formSchema";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+
+const I = styled.i`
+    padding: 0px 5px;
+`
+const DivFormGroup = styled.div`
+    width: 30%;
+    margin: 200px 35% 0;
+    padding: 0 10px;
+    border: 1px solid #173d53;
+    background-color: #25b3a7;
+    color: #173d53;
+    border-radius: 5px;
+`
+
+const DivLabel = styled.div`
+    margin: 20px 0;
+    display: flex;
+    justify-content: center;
+    width: 100%;
+`
+const DivButton = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    backgoundColor: "green";
+`
+const Button = styled.button`
+    margin: 0px auto;
+    font-size: 1.1rem;
+    padding: 2px 10px;
+`
+
+const H4 = styled.h4`
+    margin: 5px auto;
+    padding: 0;
+`
+
+const Label = styled.label`
+  font-size: 1.5rem;
+  margin: 0px auto;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`
+const Input = styled.input`
+    width: 60%;
+`
 
 const eye = <FontAwesomeIcon icon={faEye} />;
 
 const initialFormValues = {
-  email: "",
-  password: "",
+    email: "",
+    password: "",
 };
 
 const initialFormErrors = {
-  email: "",
-  password: "",
+    email: "",
+    password: "",
 };
 
 export default function LoginForm() {
-  const [formValues, setFormValues] = useState(initialFormValues);
-  const [formErrors, setFormErrors] = useState(initialFormErrors);
-  const [disabled, setDisabled] = useState(false);
-  const [passwordShown, setPasswordShown] = useState(false);
+    const [formValues, setFormValues] = useState(initialFormValues);
+    const [formErrors, setFormErrors] = useState(initialFormErrors);
+    const [disabled, setDisabled] = useState(false);
+    const [passwordShown, setPasswordShown] = useState(false);
 
-  const history = useHistory();
+    const history = useHistory();
 
-  const onSubmit = (evt) => {
-    evt.preventDefault();
-    const credentials = {
-      email: formValues.email,
-      password: formValues.password,
+    const onSubmit = (evt) => {
+        evt.preventDefault();
+        const credentials = {
+            email: formValues.email,
+            password: formValues.password,
+        };
+        // postLogin(credentials);
+        axiosWithAuth()
+            .post(
+                "https://med-cabinet-build-week.herokuapp.com/api/auth/login",
+                credentials
+            )
+            .then((res) => {
+                window.localStorage.setItem("token", res.data.token);
+                history.push("/recommend");
+            })
+            .catch((err) => {
+                console.log("Error");
+            });
     };
-    // postLogin(credentials);
-    axiosWithAuth()
-      .post(
-        "https://med-cabinet-build-week.herokuapp.com/api/auth/login",
-        credentials
-      )
-      .then((res) => {
-        window.localStorage.setItem("token", res.data.token);
-        history.push("/recommend");
-      })
-      .catch((err) => {
-        console.log("Error");
-      });
-  };
 
-  const onInputChange = (evt) => {
-    const { name, value } = evt.target;
-    Yup.reach(loginFormSchema, name)
-      .validate(value)
-      .then(() => {
-        setFormErrors({
-          ...formErrors,
-          [name]: "",
+    const onInputChange = (evt) => {
+        const { name, value } = evt.target;
+        Yup.reach(loginFormSchema, name)
+            .validate(value)
+            .then(() => {
+                setFormErrors({
+                    ...formErrors,
+                    [name]: "",
+                });
+            })
+            .catch((err) => {
+                setFormErrors({
+                    ...formErrors,
+                    [name]: err.errors[0],
+                });
+            });
+        setFormValues({
+            ...formValues,
+            [name]: value,
         });
-      })
-      .catch((err) => {
-        setFormErrors({
-          ...formErrors,
-          [name]: err.errors[0],
+    };
+
+    const togglePasswordVisiblity = () => {
+        setPasswordShown(passwordShown ? false : true);
+    };
+
+    useEffect(() => {
+        loginFormSchema.isValid(formValues).then((valid) => {
+            setDisabled(!valid);
+            //console.log('valid:' + valid);
         });
-      });
-    setFormValues({
-      ...formValues,
-      [name]: value,
-    });
-  };
+    }, [formValues]);
 
-  const togglePasswordVisiblity = () => {
-    setPasswordShown(passwordShown ? false : true);
-  };
+    return (
+        <div>
+            <form className="form container" onSubmit={onSubmit}>
+                <div className="errors">
+                    <div>{formErrors.email}</div>
+                    <div>{formErrors.password}</div>
+                </div>
 
-  useEffect(() => {
-    loginFormSchema.isValid(formValues).then((valid) => {
-      setDisabled(!valid);
-      //console.log('valid:' + valid);
-    });
-  }, [formValues]);
-
-  return (
-    <div>
-      <form className="form container" onSubmit={onSubmit}>
-        <div className="errors">
-          <div>{formErrors.email}</div>
-          <div>{formErrors.password}</div>
+                <DivFormGroup>
+                    <DivLabel>
+                        <Label>
+                            Email:&nbsp;
+                            <Input
+                                value={formValues.email}
+                                onChange={onInputChange}
+                                name="email"
+                                type="text"
+                            />
+                        </Label>
+                    </DivLabel>
+                    <DivLabel>
+                        <Label>
+                            Password:&nbsp;
+                            <Input
+                                value={formValues.password}
+                                onChange={onInputChange}
+                                name="password"
+                                type={passwordShown ? "text" : "password"}
+                            />
+                            <I onClick={togglePasswordVisiblity}>{eye}</I>
+                        </Label>
+                    </DivLabel>
+                    <DivButton>
+                        <Button disabled={disabled} >
+                            Submit
+                        </Button>
+                        <H4>
+                            Not a user?<Link className="links" to="/signup">Signup</Link>
+                        </H4>
+                    </DivButton>
+                </DivFormGroup>
+            </form>
         </div>
-
-        <div className="form-group inputs">
-          <div style={{ margin: "20px 0" }}>
-            <label>
-              Email:&nbsp;
-              <input
-                value={formValues.email}
-                onChange={onInputChange}
-                name="email"
-                type="text"
-              />
-            </label>
-          </div>
-          <div style={{ margin: "20px 0" }}>
-            <label>
-              Password:&nbsp;
-              <input
-                value={formValues.password}
-                onChange={onInputChange}
-                name="password"
-                type={passwordShown ? "text" : "password"}
-              />
-              <i onClick={togglePasswordVisiblity}>{eye}</i>
-            </label>
-          </div>
-          <div>
-            <button disabled={disabled}>Submit</button>
-          </div>
-        </div>
-      </form>
-    </div>
-  );
+    );
 }
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,7 +1,23 @@
 import React, { useState } from "react";
 import LoginForm from './LoginForm';
 import SignupForm from './SignupForm';
-import { Div, DivButton } from './LoginForm.jsx';
+import styled from "styled-components";
+
+const Div = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: Georgia;
+    color: #050709;
+    padding: 10px;
+`
+
+const DivButton = styled.div`
+    display: flex;
+    justify-content: space-around;
+    width: 40%;
+    margin: 10px 30%;
+`
 
 
 export default function LoginPage() {
